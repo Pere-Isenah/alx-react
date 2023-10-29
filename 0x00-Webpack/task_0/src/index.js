1b144ae74e35a2597bb2e18f8442216420f79d4a
Pere-Isenah <pereisenah@gmail.com>
2023-10-29 11:57:32 +0100

    task_0

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function() {
     const contents= ["Holberton Dashboard","Dashboard data for the students","Copyright - Holberton School"];
     contents.forEach(content => {
         const paragraph = $("<p>").text(content);
