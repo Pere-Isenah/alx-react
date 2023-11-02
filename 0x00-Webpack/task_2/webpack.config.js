b08ee366e2755733ad3bb202dd15992667d5d3b2
Pere-Isenah <pereisenah@gmail.com>
2023-11-02 14:49:37 +0100

    task2 update

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
           },
         {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: 'file-loader'
+        use: ['image-webpack-loader','file-loader']
         },
         ],
       },
